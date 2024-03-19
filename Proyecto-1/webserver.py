f7ff8ea11a4f5cd1c45f0539d39cc5ab95e86969
JazminAngelica <l20212436@tectijuana.edu.mx>
2024-03-19 00:54:04 +0000

    Updated to latest

--- Proyecto-1/webserver.py
+++ Proyecto-1/webserver.py
@@ -1,44 +1,12 @@
 from functools import cached_property
 from http.cookies import SimpleCookie
-from urllib.parse import parse_qsl, urlparse
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
+import redis
 
-# Código basado en:
-# https://realpython.com/python-http-server/
-# https://docs.python.org/3/library/http.server.html
-# https://docs.python.org/3/library/http.cookies.html
-
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 class WebRequestHandler(BaseHTTPRequestHandler):
-    @property
-    def query_data(self):
-        return dict(parse_qsl(self.url.query))
-    
-    @property
-    def url(self):
-        return urlparse(self.url.query)
-    
-    def search(self):
-        self.send_response(200)
-        self.send_header("content-type", "text/html")
-        self.end_headers()
-        index.page = f"<h1>{self.path}</h1>".encode("uft-8") 
-        self.wfile.write(index_page)
-    
-    def get_params(self, pattern, path):
-        match = re.match(pattern, path)
-        if match:
-            return match.groupdict()
-    
-    def index(self):
-        self.send_response(200)
-        self.send_header("content-type", "text/html")
-        self.end_headers()
-        index_page = """
-        <h1>Bienvenidos a la Biblioteca</h1>
-        form"""
-
     @cached_property
     def url(self):
         return urlparse(self.path)
@@ -47,55 +15,30 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     def query_data(self):
         return dict(parse_qsl(self.url.query))
 
-    @cached_property
-    def post_data(self):
-        content_length = int(self.headers.get("Content-Length", 0))
-        return self.rfile.read(content_length)
-
-    @cached_property
-    def form_data(self):
-        return dict(parse_qsl(self.post_data.decode("utf-8")))
-
-    @cached_property
-    def cookies(self):
-        return SimpleCookie(self.headers.get("Cookie"))
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Type", "text/html; charset=utf-8")
         self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+        books = None
+        if self.query_data and 'q' in self.query_data:
+            books = r.sinter(self.query_data['q'].split(' '))
+        self.wfile.write(self.get_response(books).encode("utf-8"))
 
-    def get_response(self):
+    def get_response(self, books):
         return f"""
     <h1> Hola Web </h1>
-    <p>  {self.path}         </p>
-    <p>  {self.headers}      </p>
-    <p>  {self.cookies}      </p>
+    <form action="/" method="get">
+    	<label for="q">  Búsqueda </label>
+    	<input type="text" name="q" required />
+	</form>
+
     <p>  {self.query_data}   </p>
+      <p>  {books}   </p>
 """
 
-
-# Hola
 
 if __name__ == "__main__":
     print("Server starting...")
     server = HTTPServer(("0.0.0.0", 8000), WebRequestHandler)
-    server.serve_forever()
-
-
-
-
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-class WebRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write('<h1>Hola {self.path}</h1>'.encode("utf-8"))
-
-if __name__ == '__main__':
-    print('Iniciando servidor...')
-    server = HTTPServer(('0.0.0.0', 8000), WebRequestHandler)
     server.serve_forever()
